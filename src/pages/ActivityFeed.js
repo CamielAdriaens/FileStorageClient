8c6d4ab12709568c12946925fe59cf3136bbfba8
CamielAdriaens <camillothee@gmail.com>
2025-01-15 21:44:38 +0100

    https>http test

--- src/pages/ActivityFeed.js
+++ src/pages/ActivityFeed.js
@@ -16,7 +16,7 @@ const ActivityFeed = ({ activities }) => {
       }
 
       const hubConnection = new signalR.HubConnectionBuilder()
-        .withUrl("https://localhost:44332/file-sharing-hub") // Your SignalR hub URL
+        .withUrl("http://localhost:8080/file-sharing-hub") // Your SignalR hub URL
         .configureLogging(signalR.LogLevel.Information) // Optional: Log SignalR messages for debugging
         .build();
 
